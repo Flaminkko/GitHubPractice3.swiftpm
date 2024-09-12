656b746d28053beceb529e8e4c399271e70a2c02
Flaminkko <olizdu123@gmail.com>
2024-09-12 11:16:40 -0500

    changed tasks to type Task and changed the strings to initilize with task, changed the List to load the .nameOfTask property

--- ContentView.swift
+++ ContentView.swift
@@ -1,11 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tasks = ["Mow the Lawn", "Feed the Dog", "Grocery Shop"]
+    @State var tasks: [Task] = [Task(nameOfTask: "Mow the Lawn"), Task(nameOfTask: "Feed the Dog"), Task(nameOfTask: "Grocery Shop")]
     var body: some View {
         List {
             ForEach(tasks, id: \.self) { task in
-                Text(task)
+                Text(task.nameOfTask)
             }
         }
     }
