59a294c2f945c05fba2551e1d0a86aa55073bbb3
Nicole Park <ypark5275@stu.d214.org>
2024-09-12 10:56:18 -0500

    created file "Task" and inside a hashable struct named "Task"

---
+++ Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  GitHubPractice3
+//
+//  Created by Yewon Park on 9/12/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct Task: Hashable {
+    var nameOfTask: String
+}
