f6b4020ece472ee94a2513ec7a1e367fa8555c71
Flaminkko <olizdu123@gmail.com>
2024-09-16 10:45:44 -0500

    formatted the date and time when adding a task

--- ContentView.swift
+++ ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
         HeaderView(tasks: $tasks, enteredDueDate: Date())
         List {
             ForEach(tasks, id: \.self) { task in
-                Text("\(task.nameOfTask) due on \(task.dueDate)")
+                Text("\(task.nameOfTask) due on \(task.dueDate.formatted(date: .numeric, time: .shortened))")
             }
         }
     }
