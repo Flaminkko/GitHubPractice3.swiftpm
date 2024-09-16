adb281a8579bc209e29079eb2dd3d215783ca730
ME <78168583+JSmskd@users.noreply.github.com>
2024-09-16 10:55:57 -0500

    Made date optional and made it look a tad bit more organised

--- ContentView.swift
+++ ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
         HeaderView(tasks: $tasks, enteredDueDate: Date())
         List {
             ForEach(tasks, id: \.self) { task in
-                Text("\(task.nameOfTask) due on \(task.dueDate.formatted(date: .numeric, time: .shortened))")
+                Text("\(task.nameOfTask)\(task.dueDate != nil ? " due on \((task.dueDate ?? Date(timeIntervalSinceReferenceDate: -123456789.0)).formatted(date: .numeric, time: .shortened))" : "")")
             }
         }
     }
--- HeaderView.swift
+++ HeaderView.swift
@@ -12,23 +12,48 @@ struct HeaderView: View {
     @Binding var tasks: [Task]
     @State var enteredString = ""
     @State var enteredDueDate: Date
+    @State var addDate = true
     var body: some View {
-        HStack {
+        VStack{
             TextField("Enter a task here", text: $enteredString)
                 .frame(width: .infinity, height: 50)
                 .padding()
                 .textFieldStyle(.roundedBorder)
+            
+            HStack{
+                Spacer()
+                Button(action: {
+                    addDate.toggle()
+                }, label: {
+                    Image(systemName: addDate ? "checkmark.square" : "xmark.square")
+                        .font(.title)
+                })
             DatePicker("Enter due date", selection: $enteredDueDate)
-            Button(action: {
-                var taskString = Task(nameOfTask: enteredString, dueDate: enteredDueDate)
-                tasks.append(taskString)
-            }, label: {
-                Text("Add Task")
-            })
-            .frame(width: 100, height: 33)
-            .background(.cyan)
-            .foregroundColor(.black)
-            .cornerRadius(7)
+                    .disabled(!addDate)
+                    .foregroundStyle(addDate ? .black : .gray)
+            Spacer()
         }
+            Rectangle().foregroundStyle(.background).frame(maxWidth:10,maxHeight: 20)
+            HStack{
+                Rectangle().foregroundStyle(.background).frame(maxWidth:40,maxHeight: 0)
+                
+                Button(action: {
+                    submit()
+                }, label: {
+                    Text("Add Task")
+                })
+                .frame(maxWidth: .infinity, maxHeight: 33)
+                .background(.cyan)
+                .foregroundColor(.black)
+                .cornerRadius(7)
+                
+                Rectangle().foregroundStyle(.background).frame(maxWidth:40,maxHeight: 0)
+            }
+            
+    }
+    }
+    func submit() {
+        tasks.append(Task(nameOfTask: enteredString, dueDate: addDate ?  enteredDueDate : nil))
+        enteredString = ""
     }
 }
